44bc07f4a1a6dcd162bc82e0ee233ec77ebf8726
Siddharth Diwan <sidwan02@gmail.com>
2021-01-19 21:45:41 -0500

    added basic style

--- app/screens/stream/screen.js
+++ app/screens/stream/screen.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { globalStyles } from '../../styles/global';
 
 // TODO
 const Stream = () => {
   return (
-    <Text>Stream screen (in screens/stream/screen.js)</Text>
+    // <Text style={globalStyles.h1}>Oh screen (in screens/stream/screen.js)</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.h1}>> ON AIR</Text>
+      <Image style={globalStyles.containerLogo} source={require('../../assets/logo/Logo.png')}/>
+      
+
+      <View style={globalStyles.containerTrackDetails}>
+        <Text style={globalStyles.headline}>Track: </Text>
+        <Text style={globalStyles.headline}>Artist:</Text>
+      </View> 
+
+      <Text style={globalStyles.h1}>> ABOUT THE SHOW</Text>
+      <View style={globalStyles. containerHostDetails}>
+        <Text style={globalStyles.headline}>Host: </Text>
+        <Text style={globalStyles.headline}>About: </Text>
+      </View> 
+    </View>
   );
 };
+
+
 
 export default Stream;
--- app/styles/global.js
+++ app/styles/global.js
@@ -1,5 +1,71 @@
 import { StyleSheet } from 'react-native';
 
 export const globalStyles = StyleSheet.create({
-  // TODO
+  h1: {
+    // fontFamily: "Courier",
+    fontWeight: "bold",
+    fontSize: 25,
+    paddingBottom: "8%"
+  },
+  h2: {
+    // fontFamily: "Courier",
+    fontWeight: "bold",
+    fontStyle: 'italic',
+    fontSize: 22
+  },
+  headline: {
+    // fontFamily: "Courier",
+    fontWeight: "bold",
+    fontSize: 14
+  },
+  body: {
+    // fontFamily: "Courier",
+    fontSize: 14
+  },
+  // color applies to both icons and text
+  colorLight: {
+    color: "#333333"
+  },
+  colorDark: {
+    color: "#F5ECD5"
+  },
+  container: {
+    backgroundColor: "#F5ECD5",
+    flex: 1, // makes the container apply to full screen not just that line
+		justifyContent: 'flex-start',
+    // alignItems: 'center',
+    paddingLeft: '5%'
+  },
+  containerLogo: {
+    backgroundColor: "#F5ECD5",
+    // flex: 1,
+    width: "20%",
+    height: "30%",
+		// justifyContent: 'flex-start',
+    // alignItems: 'center',
+    paddingLeft: '100%'
+  },
+  containerTrackDetails: {
+    backgroundColor: "#F5ECD5",
+    // flex: 1,
+		justifyContent: 'flex-start',
+    // alignItems: 'center',
+    paddingLeft: '25%',
+    paddingTop: "10%",
+    paddingBottom: "15%"
+  },
+  containerHostDetails: {
+    backgroundColor: "#F5ECD5",
+    flex: 1,
+		justifyContent: 'flex-start',
+    // alignItems: 'center',
+    paddingLeft: '15%',
+  }
+  
+  // background: {
+  //   backgroundColor: "#FAF8F0",
+  // }, 
+  // container: {
+  //   marginHorizontal: "8%",
+  // }, 
 });
