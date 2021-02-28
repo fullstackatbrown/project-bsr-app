f5caf6b0b612c06f974593a42d2346e0b11a33c5
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-02-28 12:43:39 -0800

    Add loadscreen animation

--- app/App.js
+++ app/App.js
@@ -23,8 +23,8 @@ const App = () => {
     return new Promise(resolve => {
       setTimeout(() => {
         resolve('resolved');
-      // }, 2000);
-      }, 0);
+      }, 2000);
+      // }, 0);
     });
   }
 
@@ -33,8 +33,8 @@ const App = () => {
     // eg: pass setDisplayLoading in as a prop and set to false when "reload" button clicked
     if (displayLoading) {
       getData().then(() => {
-        // setDisplayLoading(true);
-        setDisplayLoading(false);
+        setDisplayLoading(true);
+        // setDisplayLoading(false);
         setLoadingError(false);
       }).catch(() => {
         setLoadingError(true);
