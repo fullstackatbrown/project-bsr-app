54b6a274410d2822bd58f0f1016154f396e5fba2
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-05-02 00:55:26 -0700

    Fetching host names partially works

--- app/screens/schedule/ScheduleCard.js
+++ app/screens/schedule/ScheduleCard.js
@@ -1,64 +1,159 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { globalStyles } from '../../styles/global';
 import { WebView } from 'react-native-webview';
 
-const ScheduleCard = ({time, host, show, about}) => {
+const ScheduleCard = ({time, hostLinks, show, about}) => {
 
   const [showAll, setShowAll] = useState(false);
+  const [hostNames, setHostNames] = useState([]);
 
-  function toggleCollapse(){
+  const getHostData = (url) => {
+    const data = fetch(url)
+        .then(response => response.json())
+    return data;
+  }
+
+  // useEffect(() => {
+    // console.log(JSON.stringify(hostLinks));
+    // console.log(hostLinks[0].href);
+    // for (let i = 0; i < hostLinks.length; i++) {
+    //   console.log(hostLinks[0].href);
+    // }
+    
+    // if (hostLinks) {
+
+      // let names = ["hi", "temp"];
+      // let names = [];
+      // hostLinks.forEach(async (aLink) => {
+      //   console.log(aLink.href);
+
+      //   const response = await fetch(aLink.href);
+      //   const data = await response.json();
+
+      //   console.log(data.name);
+      //   const newName = data.name;
+      //   if (!hostNames.includes(newName)) {
+      //     let temp = hostNames;
+      //     temp.push(newName);
+      //     setHostNames(temp);
+      //   }
+      //   getHostData(aLink.href).then(data => {
+      //     console.log("data: ");
+      //     console.log(data.name)
+      //     let newName = [hostNames, data.name].join(", ");
+      //     setHostNames(newName);
+      //     // names.push(data.name);
+      //   }).catch((err) => {
+      //     console.log(err);
+      //   });
+      // });
+
+
+
+      // const url = hostLinks[0].href;
+
+      // getHostData(url).then(data => {
+      //   setHostNames(data.name);
+      // }).catch((err) => {
+      //   console.log(err);
+      // });
+
+      // let ret = names.join(", ");
+      // setHostNames(ret);
+  //   }
+
+  // }, []);
+
+
+  useEffect(() => {
+    setHostNames([]);
+    if (hostLinks) {
+      // let temp = hostLinks.map((aLink) => aLink.href);
+      // setHostNames([...hostLinks]);
+      // return;
+
+      hostLinks.forEach(async (aLink) => {
+        console.log("link: " + aLink.href);
+
+        const response = await fetch(aLink.href);
+        const data = await response.json();
+
+        console.log(data.name);
+        const newName = data.name;
+        if (!hostNames.includes(newName)) {
+          let temp = [...hostNames];
+          temp.push(newName);
+          setHostNames(temp);
+        }
+      })
+    }
+  }, [hostLinks])
+
+  function toggleCollapse() {
     setShowAll(!showAll);
   }
 
+  function stripParagraphTags(textValue) {
+    if (textValue.length <= 7) { // "<p></p>".length
+      return textValue;
+    }
+
+    if (textValue.substring(0, 3) === "<p>") {
+      textValue = textValue.substring(3);
+    }
+
+    if (textValue.substring(textValue.length - 4, textValue.length) === "</p>"){
+      textValue = textValue.substring(0, textValue.length - 4);
+    }
+
+    return textValue;
+  }
+
   return (
-    <View style={styles.cardContainer}>
-      <View style={styles.timeContainer}>
-        <Text style={globalStyles.h2}>{time}</Text>
-      </View>
+    <View style={styles.centeringContainer}>
+      <View style={styles.cardContainer}>
+        <View style={styles.timeContainer}>
+          <Text style={globalStyles.h2}>{time}</Text>
+        </View>
 
-      <Text>
-        <Text style={globalStyles.headline}>Host: </Text>{host}
-      </Text>
+        <Text>
+          <Text style={globalStyles.headline}>Host: </Text>{hostNames.join(", ")}
+        </Text>
 
-      <Text>
-        <Text style={globalStyles.headline}>Show: </Text>{show}
-      </Text>
+        <Text>
+          <Text style={globalStyles.headline}>Show: </Text>{show}
+        </Text>
 
-      <Text numberOfLines={showAll ? 0 : 3}>
-        <Text style={globalStyles.headline}>About: </Text>{about}
-      </Text>
+        { (about.length > 0) && 
+        <>
+          <Text numberOfLines={showAll ? 0 : 3}>
+            <Text style={globalStyles.headline}>About: </Text>{stripParagraphTags(about)}
+          </Text>
 
-      <View style={styles.toggleButtonContainer}>
-        <MaterialIcons name={showAll ? 'expand-less' : 'expand-more'} size={40} onPress={toggleCollapse} />
+          <View style={styles.toggleButtonContainer}>
+            <MaterialIcons name={showAll ? 'expand-less' : 'expand-more'} size={40} onPress={toggleCollapse} />
+          </View>
+        </>
+        }
       </View>
     </View>
   );
 };
 
-// const WebViewWrapper = () => {
-//     // <WebView
-//     //   originWhitelist={['*']}
-//     //   source={{ html: '<h1>Hello world</h1>' }}
-//     //   style={{ marginTop: 20 }}
-//     // />
-//   function renderHtmlText() {
-//     // return "<b> hello mortal </b>";
-//     return {__html: 'First &middot; Second'};
-
-//   }
-//   // return <div dangerouslySetInnerHTML={renderHtmlText()}/>;
-//   // return <div dangerouslySetInnerHTML={renderHtmlText()} />;
-//   return <div dangerouslySetInnerHTML={__html: {'<b> hti </b>'}} />;
-
-// }
-
 const styles = StyleSheet.create({
+  centeringContainer: {
+    width: "100%",
+    justifyContent: "center", 
+    alignItems: "center"
+  },
   cardContainer: {
-    paddingHorizontal: "2%", 
+    paddingHorizontal: "4%", 
+    width: "100%",
     borderColor: "black", 
     borderTopWidth: 1.4, 
+    marginVertical: "2%", 
   }, 
   boldedText: {
     fontWeight: "bold"
--- app/screens/schedule/screen.js
+++ app/screens/schedule/screen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
 import ScheduleCard from './ScheduleCard';
 import DropDown from './DropDown';
 import { globalStyles } from '../../styles/global';
@@ -8,17 +8,11 @@ import { DataContext } from '../../src/DataContext';
 
 // TODO
 const Schedule = () => {
-  const loremIpsum = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ";
-  const dummyDayData = [
-    {time: "8-9PM ET", host: "Caitlin Pintavorn", show: "Flip Side", about: loremIpsum}, 
-    {time: "9-10PM ET", host: "Ricky hage, calista shang", show: "TBH", about: loremIpsum}]
+  const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-  const daysData = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-  const dummyData = daysData.map((value, index) => {
+  const dummyData = dayNames.map((value, index) => {
     var dayData = [];
-    var placeholder = {time: `8-9PM ET (${value})`, host: "N/A", show: "Flip Side", about: loremIpsum};
-
+    var placeholder = {time: `8-9PM ET (${value})`, host: "N/A", show: "Flip Side", about: "hello"};
     for(var i = 0; i < index + 1; i++){
       dayData.push(placeholder);
     }
@@ -27,31 +21,47 @@ const Schedule = () => {
 
   const [data, setData] = useState(dummyData);
   const [day, setDay] = useState(0);
-
   const currentAppData = useContext(DataContext);
 
   useEffect(() => {
-    console.log("currentShowData (in screen): " + JSON.stringify(currentAppData, null, 2));
-    // console.log(currentShowData.showData.items[0]);
-
-    const _dummyData = daysData.map((value, index) => {
+    const _dummyData = dayNames.map((value, index) => {
       var dayData = [];
       currentAppData.showData.items.forEach((aShow) => {
-        var placeholder = {time: `8-9PM ET (MONDAy)`, host: "Caitlin Pintavorn", show: aShow.title, about: aShow.description};
+        // format: 2021-05-05T22:00:00+0000
+        let startTimeString = aShow.start.slice(0, (aShow.start.length - 2)) + ":" + aShow.start.slice(aShow.start.length - 2);
+        let startTime = new Date(startTimeString);
+        if (startTime.getDay() !== index) {
+          return;
+        }
+
+        let endTimeString = aShow.end.slice(0, (aShow.end.length - 2)) + ":" + aShow.end.slice(aShow.end.length - 2);
+        let endTime = new Date(endTimeString);
+
+        let timezone = startTime.toString().match(/\(([A-Za-z\s].*)\)/)[1];
+        let startString = startTime.toLocaleString('en-US', { hour: 'numeric', hour12: true })
+        let endString = endTime.toLocaleString('en-US', { hour: 'numeric', hour12: true })
+        
+        var placeholder = {
+          time: `${startString}-${endString} ${timezone}`, 
+          hostLinks: aShow["_links"].personas, 
+          show: aShow.title, 
+          about: aShow.description
+        };
         dayData.push(placeholder);
+
       });
       return dayData;
     });
     setData(_dummyData);
-  }, [])
+  }, [currentAppData.showData])
 
-  const pickerItems = daysData.map((value, index) => {
-    return { label: daysData[index], value: index, key: index };
+  const pickerItems = dayNames.map((value, index) => {
+    return { label: dayNames[index], value: index, key: index };
   });
 
   return (
     <ScrollView>
-      <View style={globalStyles.container}>
+      <View style={styles.centeringContainer}>
 
         <Text style={globalStyles.h1}>>&nbsp;&nbsp;SHOW SCHEDULE</Text>
 
@@ -65,22 +75,23 @@ const Schedule = () => {
           return <ScheduleCard 
             key={index}
             time={value.time} 
-            host={value.host} 
+            hostLinks={value.hostLinks} 
             show={value.show}
             about={value.about}
           />
         })}
 
-        <Text>
-          show data
-          {
-          JSON.stringify(currentAppData)
-          }
-        </Text>
-
       </View>
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  centeringContainer: {
+    width: "100%",
+    justifyContent: "center",
+    paddingHorizontal: '5%'
+  }
+});
 
 export default Schedule;
